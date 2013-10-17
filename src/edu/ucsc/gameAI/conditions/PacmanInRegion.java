8d50a2132a892997affa92c6a6f9281d71c145c3
LochNeffMonster <atneff@ucsc.edu>
2013-10-16 19:54:21 -0700

    Updated *InRegion conditions to match Condition Interface.

--- src/edu/ucsc/gameAI/conditions/PacmanInRegion.java
+++ src/edu/ucsc/gameAI/conditions/PacmanInRegion.java
@@ -5,23 +5,21 @@ import pacman.game.Game;
 
 public class PacmanInRegion implements ICondition {
     
-    private Game _game;
     private int _maxX, _maxY, _minX,_minY;
     
-    public PacmanInRegion(Game game, int x1, int y1, int x2, int y2)
+    public PacmanInRegion(int x1, int y1, int x2, int y2)
     {
-        _game = game;
         _maxX = (x1 > x2) ? x1 : x2;
         _minX = (x1 > x2) ? x2 : x1;
         _maxY = (y1 > y2) ? y1 : y2;
         _minY = (y1 > y2) ? y2 : y1;
     }
     
-    public boolean test() 
+    public boolean test(Game game) 
     {
-    	int index = _game.getPacmanCurrentNodeIndex();
-    	int x = _game.getNodeXCood(index);
-    	int y = _game.getNodeYCood(index);
+    	int index = game.getPacmanCurrentNodeIndex();
+    	int x = game.getNodeXCood(index);
+    	int y = game.getNodeYCood(index);
     	boolean insideX = (x <= _maxX && x >= _minX);
     	boolean insideY = (y <= _maxY && y >= _minY);
         return (insideX && insideY);
--- src/edu/ucsc/gameAI/conditions/PowerPillInRegion.java
+++ src/edu/ucsc/gameAI/conditions/PowerPillInRegion.java
@@ -5,24 +5,22 @@ import pacman.game.Game;
 
 public class PowerPillInRegion implements ICondition {
     
-    private Game _game;
-private int _maxX, _minX, _maxY, _minY;
+    private int _maxX, _minX, _maxY, _minY;
     
     public PowerPillInRegion(Game game, int x1, int y1, int x2, int y2)
     {
-        _game = game;
         _maxX = (x1 > x2) ? x1 : x2;
         _minX = (x1 > x2) ? x2 : x1;
         _maxY = (y1 > y2) ? y1 : y2;
         _minY = (y1 > y2) ? y2 : y1;
     }
     
-    public boolean test() 
+    public boolean test(Game game) 
     {
-    	int[] pillIndices = _game.getActivePowerPillsIndices();
+    	int[] pillIndices = game.getActivePowerPillsIndices();
     	for (int index : pillIndices){
-    		int x = _game.getNodeXCood(index);
-        	int y = _game.getNodeYCood(index);
+    		int x = game.getNodeXCood(index);
+        	int y = game.getNodeYCood(index);
         	boolean insideX = (x <= _maxX && x >= _minX);
         	boolean insideY = (y <= _maxY && y >= _minY);
             if (insideX && insideY) return true;
